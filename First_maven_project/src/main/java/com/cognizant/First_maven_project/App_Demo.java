3db22bedbbe23333156cb172173157dd1e9ddbdb
jamdar123 <djamdar122@gmail.com>
2020-11-20 02:53:12 -0800

    commuted all repo

---
+++ First_maven_project/src/main/java/com/cognizant/First_maven_project/App_Demo.java
@@ -0,0 +1,12 @@
+package com.cognizant.First_maven_project;
+
+import javax.swing.Spring;
+
+public class App_Demo {
+
+	public String returnsomething()
+	{
+		return "Hello Dinesh";
+	}
+
+}
---
+++ First_maven_project/src/test/java/com/cognizant/First_maven_project/App_test.java
@@ -0,0 +1,22 @@
+package com.cognizant.First_maven_project;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class App_test{
+	
+	@Test
+	public void Test()
+	{
+		fail();
+		
+	}
+	
+	
+	
+	
+
+}
